644ffea00b09f61b27c726ae341395105a424149
Szczepan Faber <szczepiq@users.noreply.github.com>
2008-08-08 16:03:28 +0000

    on the way to change how statics are mocked. Trying to implement when(Dog).bark().thenReturn("foo") / unstub(Dog) syntax for stubbing statics. added lots of dodgy printlns to allow me understand what did we write... :D

--- mockito_staticmethods_test.py
+++ mockito_staticmethods_test.py
@@ -5,6 +5,10 @@ class Dog():
   @staticmethod
   def bark():
     return "woof!"
+  
+  @staticmethod
+  def barkHardly(*args):
+    return "woof woof!"
 
 class Cat():
   @staticmethod
@@ -13,24 +17,47 @@ class Cat():
 
 class MockitoStaticmethodsTest(TestBase):   
 
-  def testVerifiesSuccesfully(self):     
-    dog = ClassMock(Dog)
-    when(dog).bark().thenReturn("miau!")
+  def testStubs(self):     
+    self.assertEquals("woof!", Dog.bark())
+    
+    when(Dog).bark().thenReturn("miau!")
+    
+    self.assertEquals("miau!", Dog.bark())
+    
+  def testStubsWithArgs(self):     
+    self.assertEquals("woof woof!", Dog.barkHardly(1, 2))
+    
+    when(Dog).barkHardly(1, 2).thenReturn("miau!")
+    
+    self.assertEquals("miau!", Dog.barkHardly(1, 2))
 
-    Dog.bark()
+  def xtestStubsButDoesNotMachArguments(self): 
+    self.assertEquals("woof woof!", Dog.barkHardly(1, "anything"))
+    
+    when(Dog).barkHardly(1, 2).thenReturn("miau!")
+    
+    self.assertEquals(None, Dog.barkHardly(1))
+
+  def xtestUnstubs(self):     
+    when(Dog).bark().thenReturn("miau!")
+    unstub(Dog)
+    self.assertEquals("woof!", Dog.bark())
+    
+  def xtestVerifiesSuccesfully(self):     
+    when(Dog).bark().thenReturn("miau!")
+    
     Dog.bark()
     
-    verify(dog, times(2)).bark()
+    verify(Dog).bark()
     
-  def testFailsVerification(self):
-    dog = ClassMock(Dog)
-    when(dog).bark().thenReturn("miau!")
+  def xtestFailsVerification(self):
+    when(Dog).bark().thenReturn("miau!")
 
     Dog.bark()
     
-    self.assertRaises(VerificationError, verify(dog, times(2)).bark)
+    self.assertRaises(VerificationError, verify(Dog, times(2)).bark)
 
-  def testStubsAndVerifiesClassmethod(self):
+  def xtestStubsAndVerifiesClassmethod(self):
     dog = ClassMock(Dog)
     when(dog).bark().thenReturn("miau!")
     
@@ -47,3 +74,6 @@ class MockitoStaticmethodsTest(TestBase):
   #  
   #  self.assertEquals("miau!", Dog.bark())
   #  self.assertEquals("woof!", Cat.meow())
+
+if __name__ == '__main__':
+  unittest.main()
