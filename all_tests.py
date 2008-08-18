d84306d55e219972da1696a0861904841452c7bd
Bartosz Bańkowski <bbankowski@users.noreply.github.com>
2008-08-18 16:12:44 +0000

    Changes in test runner. One TODO comment added.

--- all_tests.py
+++ all_tests.py
@@ -5,13 +5,11 @@ from mockito_staticmethods_test import *
 from mockito_stubbing_test import * 
 from mockito_verification_test import * 
 
-#TODO dodgy, use loading all test from module
-t1 = unittest.TestLoader().loadTestsFromTestCase(MockitoClassMethodsTest)
-t2 = unittest.TestLoader().loadTestsFromTestCase(MockitoMatchersTest)
-t3 = unittest.TestLoader().loadTestsFromTestCase(MockitoStaticMethodsTest)
-t4 = unittest.TestLoader().loadTestsFromTestCase(MockitoStubbingTest)
-t5 = unittest.TestLoader().loadTestsFromTestCase(MockitoVerificationTest)
+tests = [MockitoClassMethodsTest, MockitoMatchersTest, MockitoStaticMethodsTest, 
+         MockitoStubbingTest, MockitoVerificationTest]
 
-all = unittest.TestSuite([t1, t2, t3, t4, t5])
+all = unittest.TestSuite()
+for test in tests:
+    all.addTest(unittest.makeSuite(test))
 
 unittest.TextTestRunner(verbosity=2).run(all)
