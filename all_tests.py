992d8ae0a51a3c132bd45ab2833135a63d27260b
Bartosz Bańkowski <bbankowski@users.noreply.github.com>
2008-08-19 13:38:46 +0000

    Changing nomenclature.

--- all_tests.py
+++ all_tests.py
@@ -4,10 +4,10 @@ from mockito_matchers_test import *
 from mockito_staticmethods_test import * 
 from mockito_stubbing_test import * 
 from mockito_verification_test import * 
-from mockito_modulemethods_test import * 
+from mockito_modulefunctions_test import * 
 
 tests = [MockitoClassMethodsTest, MockitoMatchersTest, MockitoStaticMethodsTest, 
-         MockitoStubbingTest, MockitoVerificationTest, MockitoModuleMethodsTest]
+         MockitoStubbingTest, MockitoVerificationTest, MockitoModuleFunctionsTest]
 
 all = unittest.TestSuite()
 for test in tests: all.addTest(unittest.makeSuite(test))
