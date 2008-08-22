d7c88876deb3eb3aab280b2466d1e94f20329a21
szczepiq <szczepiq@localhost>
2008-08-22 20:51:35 +0000

    added TODOs

--- mockito_staticmethods_test.py
+++ mockito_staticmethods_test.py
@@ -25,6 +25,7 @@ class MockitoStaticMethodsTest(TestBase):
     unstub()
     self.assertEquals("woof", Dog.bark())
 
+#TODO decent test case please :) without testing irrelevant implementation details
   def testUnstubShouldPreserveMethodType(self):
     when(Dog).bark().thenReturn("miau!")
     unstub()
