f0b52ee3b6e19bb7eba1d810877ab7df6503e8ff
Szczepan Faber <szczepiq@users.noreply.github.com>
2008-08-23 16:23:34 +0000

    some further refactorings to increase readability

--- static_mocker.py
+++ static_mocker.py
@@ -12,25 +12,28 @@ class StaticMocker():
   def stub(self, invocation):
     self.static_mocks[invocation.getMockedObj()] = invocation.mock
     original_method = invocation.getRealMethod()
-    # TODO original should hold entire invocation?    
-    original = (invocation.getMockedObj(), invocation.method_name, original_method)
+    original = (original_method, invocation)
+    self.originals.append(original)
 
-    def mocked_method(*params, **named_params): 
+    self._replaceMethod(invocation, original_method)
+    
+  def _replaceMethod(self, invocation, original_method):
+    
+    def new_mocked_method(*params, **named_params): 
       if self._is_classmethod(original_method): params = params[1:]
       i = invocation.mock.__getattr__(invocation.method_name)
       return i.__call__(*params, **named_params)
       
-    self.originals.append(original)
     # TODO questions should be asked on invocation object?    
     if self._is_staticmethod(original_method):
-      invocation.replaceMethod(staticmethod(mocked_method))
+      invocation.replaceMethod(staticmethod(new_mocked_method))
     elif self._is_classmethod(original_method): 
-      invocation.replaceMethod(classmethod(mocked_method))
+      invocation.replaceMethod(classmethod(new_mocked_method))
     elif inspect.ismodule(invocation.getMockedObj()):
-      invocation.replaceMethod(mocked_method)
+      invocation.replaceMethod(new_mocked_method)
     else:
       # TODO create decent error. is it necessary? this case is only useful for library debugging        
-      raise "Only modules functions, static and class methods can be stubbed"    
+      raise "Only module functions, static and class methods can be stubbed"  
     
   def _is_classmethod(self, method):
     return isinstance(method, classmethod)
@@ -46,5 +49,5 @@ class StaticMocker():
   
   def unstub(self):
     while self.originals:
-      cls, original_method_name, original_method = self.originals.pop()
-      setattr(cls, original_method_name, original_method)
+      original_method, invocation = self.originals.pop()
+      invocation.replaceMethod(original_method)
