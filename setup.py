0f3181ac3f5ec6e77eae5ef5835b680f49588e38
szczepiq <szczepiq@localhost>
2008-09-14 20:32:10 +0000

    Added setup script to install the library

---
+++ setup.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python2.4
+
+from distutils.core import setup
+
+setup(name='mockito',
+      version='0.1.0',
+      py_modules=['mockito', 'matchers', 'static_mocker'],
+      url='http://code.google.com/p/mockito/wiki/MockitoForPython',
+      maintainer='mockito maintainers',
+      maintainer_email='mockito-python@googlegroups.com',
+      license='MIT',
+      description='Spying framework',
+      long_description='Mockito is a spying framework based on Java library with the same name.'
+)
