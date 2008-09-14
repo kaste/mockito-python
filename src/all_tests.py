f1beaf4b74b56f8473bb8154b5d008a6df9c7a42
szczepiq <szczepiq@localhost>
2008-09-14 21:20:19 +0000

    put sources in src folder

---
+++ src/all_tests.py
@@ -0,0 +1,14 @@
+import unittest
+from mockito_classmethods_test import * 
+from mockito_matchers_test import * 
+from mockito_staticmethods_test import * 
+from mockito_stubbing_test import * 
+from mockito_verification_test import * 
+from mockito_modulefunctions_test import * 
+
+tests = [MockitoClassMethodsTest, MockitoMatchersTest, MockitoStaticMethodsTest, 
+         MockitoStubbingTest, MockitoVerificationTest, MockitoModuleFunctionsTest]
+
+all = unittest.TestSuite()
+for test in tests: all.addTest(unittest.makeSuite(test))
+unittest.TextTestRunner(verbosity=2).run(all)
