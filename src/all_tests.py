af5faf83ed87f17746658b9e8ea4c44108d8b5ec
szczepiq <szczepiq@localhost>
2008-09-14 21:40:32 +0000

    added representative test that will be part of the documentation

--- src/all_tests.py
+++ src/all_tests.py
@@ -4,10 +4,12 @@ from mockito_matchers_test import *
 from mockito_staticmethods_test import * 
 from mockito_stubbing_test import * 
 from mockito_verification_test import * 
-from mockito_modulefunctions_test import * 
+from mockito_modulefunctions_test import *
+from mockito_representative_test import *  
 
 tests = [MockitoClassMethodsTest, MockitoMatchersTest, MockitoStaticMethodsTest, 
-         MockitoStubbingTest, MockitoVerificationTest, MockitoModuleFunctionsTest]
+         MockitoStubbingTest, MockitoVerificationTest, MockitoModuleFunctionsTest,
+         MockitoRepresentativeTest]
 
 all = unittest.TestSuite()
 for test in tests: all.addTest(unittest.makeSuite(test))
