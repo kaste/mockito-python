7aeacf15c9f911b85c60181e5d7d77add0d97b3b
Szczepan Faber <szczepiq@gmail.com>
2008-09-22 22:29:54 +0200

    verification errors print string arguments nicer HG: changed src/mockito_verification_errors_test.py

--- src/mockito_verification_errors_test.py
+++ src/mockito_verification_errors_test.py
@@ -16,7 +16,15 @@ class MockitoVerificationErrorsTest(TestBase):
       verify(mock).foo(1, 2)
     except VerificationError, e:
       pass
-    self.assertEquals("\nWanted but not invoked: foo(1, 2)", e.message)      
+    self.assertEquals("\nWanted but not invoked: foo(1, 2)", e.message)
+    
+  def testVerificationErrorPrintsNicelyStringArguments(self):
+    mock = Mock()
+    try:
+      verify(mock).foo(1, 'foo')
+    except VerificationError, e:
+      pass
+    self.assertEquals("\nWanted but not invoked: foo(1, 'foo')", e.message)         
 
 if __name__ == '__main__':
   unittest.main()
