12e31f3d71b72e22c5c56c76ceb11c6c5f54cc6d
Szczepan Faber <szczepiq@users.noreply.github.com>
2008-09-22 22:29:54 +0200

    verification errors print string arguments nicer HG: changed src/mockito_verification_errors_test.py

--- src/method_printer.py
+++ src/method_printer.py
@@ -2,6 +2,10 @@ class MethodPrinter:
   def printIt(self, method_name, *params):
     m = method_name + "("
     for p in params:
-      m += str(p) + ", "
+      m += self.to_str(p) + ", "
     #TODO be smarter here, a search and replace?      
     return m.rstrip(", ") + ")"
+  
+  def to_str(self, obj):
+    if str == type(obj): return "'" + obj + "'"
+    else: return str(obj)
--- src/method_printer_test.py
+++ src/method_printer_test.py
@@ -5,9 +5,8 @@ class MethodPrinterTest(TestBase):
   
   def testPrintsMethod(self):
     self.assertEquals("foo()", MethodPrinter().printIt("foo"))
-    
-  def testPrintsMethodAndArgs(self):
     self.assertEquals("foo(1, 2)", MethodPrinter().printIt("foo", 1, 2))    
+    self.assertEquals("foo(1, 'bar')", MethodPrinter().printIt("foo", 1, 'bar'))    
     
 if __name__ == '__main__':
   unittest.main()
