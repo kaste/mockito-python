eb935fa76cffe8bd10cac4c8cfe200a3831f851d
szczepiq <szczepiq@localhost>
2008-10-21 23:54:41 +0200

    fixed tests so that it all works fine with python 2.4

--- src/mockito_verification_errors_test.py
+++ src/mockito_verification_errors_test.py
@@ -8,21 +8,21 @@ class MockitoVerificationErrorsTest(TestBase):
     try:
       verify(mock).foo()
     except VerificationError, e:
-      self.assertEquals("\nWanted but not invoked: foo()", e.message)
+      self.assertEquals("\nWanted but not invoked: foo()", str(e))
       
   def testPrintsNicelyArguments(self):
     mock = Mock()
     try:
       verify(mock).foo(1, 2)
     except VerificationError, e:
-      self.assertEquals("\nWanted but not invoked: foo(1, 2)", e.message)
+      self.assertEquals("\nWanted but not invoked: foo(1, 2)", str(e))
     
   def testPrintsNicelyStringArguments(self):
     mock = Mock()
     try:
       verify(mock).foo(1, 'foo')
     except VerificationError, e:
-      self.assertEquals("\nWanted but not invoked: foo(1, 'foo')", e.message)
+      self.assertEquals("\nWanted but not invoked: foo(1, 'foo')", str(e))
     
   def testPrintsUnwantedInteraction(self):
     mock = Mock()
@@ -30,7 +30,7 @@ class MockitoVerificationErrorsTest(TestBase):
     try:
       verifyNoMoreInteractions(mock)
     except VerificationError, e:
-      self.assertEquals("\nUnwanted interaction: foo(1, 'foo')", e.message)
+      self.assertEquals("\nUnwanted interaction: foo(1, 'foo')", str(e))
 
 if __name__ == '__main__':
   unittest.main()
