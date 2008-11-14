026dcc50e1d021a66ec03339dfdce0f77491a6a4
Szczepan Faber <szczepiq@gmail.com>
2008-11-14 22:12:54 +0100

    merged Bartek changes

--- src/all_tests.py
+++ src/all_tests.py
@@ -1,18 +1,12 @@
 import unittest
-from mockito_classmethods_test import * 
-from mockito_matchers_test import * 
-from mockito_staticmethods_test import * 
-from mockito_stubbing_test import * 
-from mockito_verification_test import * 
-from mockito_modulefunctions_test import *
-from mockito_demo_test import *  
-from mockito_verification_errors_test import *  
+import os
+import re
 
-#TODO: can this be smarter - i can forget to put a test class here:
-tests = [MockitoClassMethodsTest, MockitoMatchersTest, MockitoStaticMethodsTest, 
-         MockitoStubbingTest, MockitoVerificationTest, MockitoModuleFunctionsTest,
-         MockitoDemoTest, MockitoVerificationErrorsTest]
+pattern = re.compile('([a-z]+_)+test\.py$')
 
-all = unittest.TestSuite()
-for test in tests: all.addTest(unittest.makeSuite(test))
-unittest.TextTestRunner(verbosity=2).run(all)
+loader = unittest.TestLoader()
+suite = unittest.TestSuite()
+
+names = [f.replace('.py', '') for f in os.listdir('.') if pattern.match(f, 1)]   
+for name in names: suite.addTests(loader.loadTestsFromName(name))
+unittest.TextTestRunner(verbosity=2).run(suite)
