0c7cdf28e9bf57500cc1bb9340a751436941f142
Szczepan Faber <szczepiq@users.noreply.github.com>
2008-11-16 00:38:55 +0100

    some rename job

--- src/static_mocker.py
+++ src/static_mocker.py
@@ -8,28 +8,28 @@ class StaticMocker:
   def __init__(self):
     self.originals = []
     self.static_mocks = {}
-
-  def stub(self, invocation):
-    self.static_mocks[invocation.getMockedObj()] = invocation.mock
-    original_method = invocation.getRealMethod()
-    original = (original_method, invocation)
+    
+  def stub(self, stubbed_invocation):
+    self.static_mocks[stubbed_invocation.mock.mocked_obj] = stubbed_invocation.mock
+    original_method = stubbed_invocation.getOriginalMethod()
+    original = (original_method, stubbed_invocation)
     self.originals.append(original)
 
-    self._replaceMethod(invocation, original_method)
+    self._replaceMethod(stubbed_invocation, original_method)
     
-  def _replaceMethod(self, invocation, original_method):
+  def _replaceMethod(self, stubbed_invocation, original_method):
     
     def new_mocked_method(*params, **named_params): 
       if self._is_classmethod(original_method): params = params[1:]
-      call = invocation.mock.__getattr__(invocation.method_name)
+      call = stubbed_invocation.mock.__getattr__(stubbed_invocation.method_name)
       return call(*params, **named_params)
       
     if self._is_staticmethod(original_method):
-      invocation.replaceMethod(staticmethod(new_mocked_method))
+      stubbed_invocation.replaceMethod(staticmethod(new_mocked_method))
     elif self._is_classmethod(original_method): 
-      invocation.replaceMethod(classmethod(new_mocked_method))
-    elif inspect.ismodule(invocation.getMockedObj()):
-      invocation.replaceMethod(new_mocked_method)
+      stubbed_invocation.replaceMethod(classmethod(new_mocked_method))
+    elif inspect.ismodule(stubbed_invocation.mock.mocked_obj):
+      stubbed_invocation.replaceMethod(new_mocked_method)
     else:
       # TODO create decent error. is it necessary? this case is only useful for library debugging        
       raise "Only module functions, static and class methods can be stubbed"  
@@ -48,5 +48,5 @@ class StaticMocker:
   
   def unstub(self):
     while self.originals:
-      original_method, invocation = self.originals.pop()
-      invocation.replaceMethod(original_method)
+      original_method, stubbed_invocation = self.originals.pop()
+      stubbed_invocation.replaceMethod(original_method)
