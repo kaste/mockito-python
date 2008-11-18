6f3169d691f2f735307038c4f07f8ecedfe49443
Szczepan Faber <szczepiq@users.noreply.github.com>
2008-11-18 23:37:20 +0100

    moved some code to the verification module

--- build/lib/src/mockito.py
+++ build/lib/src/mockito.py
@@ -1,172 +1,38 @@
 import matchers
-from static_mocker import *
-
-_STUBBING_ = -2
-
-_STATIC_MOCKER_ = StaticMocker()
-
-_RETURNS_ = 1
-_THROWS_ = 2
-
-class Mock:
-  
-  def __init__(self):
-    self.invocations = []
-    self.stubbed_invocations = []
-    self.mocking_mode = None
-    self.mocked_obj = None
-  
-  def __getattr__(self, method_name):
-    if self.isStubbing() or self.isStubbingStatic():
-      return InvocationStubber(self, method_name)
-    
-    if self.mocking_mode >= 0:
-      return InvocationVerifier(self, method_name)
-      
-    return InvocationMemorizer(self, method_name)
-  
-  def isStubbingStatic(self):
-    return self.isStubbing() and _STATIC_MOCKER_.accepts(self.mocked_obj) 
-  
-  def isStubbing(self):
-    return self.mocking_mode == _STUBBING_
-  
-  def finishStubbing(self, invocation):
-    if (self.stubbed_invocations.count(invocation)):
-      self.stubbed_invocations.remove(invocation)
-      
-    self.stubbed_invocations.append(invocation)
-    
-    if (self.isStubbingStatic()):
-      _STATIC_MOCKER_.stub(invocation)
-      
-    self.mocking_mode = None
-    
-class Invocation:
-  def __init__(self, mock, method_name):
-    self.method_name = method_name
-    self.mock = mock
-    self.answers = []
-    self.verified = False
-    self.params = ()
-  
-  def getMockedObj(self):
-    return self.mock.mocked_obj
-  
-  def getRealMethod(self):
-    return self.getMockedObj().__dict__.get(self.method_name)
-  
-  def replaceMethod(self, new_method):
-    setattr(self.getMockedObj(), self.method_name, new_method)
-    
-  def __cmp__(self, other):
-    if self.matches(other): return 0
-    else: return 1
-    
-  def matches(self, invocation):
-    if self.method_name == invocation.method_name and self.params == invocation.params:
-      return True
-    if len(self.params) != len(invocation.params):
-      return False
-    return self.__compareUsingMatchers(invocation)
-
-  def __compareUsingMatchers(self, invocation):  
-    for x, p1 in enumerate(self.params):
-      p2 = invocation.params[x]
-      if isinstance(p1, matchers.Matcher):
-        if not p1.matches(p2): return False
-      elif p1 != p2: return False
-    return True
-  
-  def stubWith(self, answer, chained_mode):
-    if chained_mode:
-        self.answers[-1].append(answer.current())
-    else:
-        self.answers.append(answer)
-        
-    self.mock.finishStubbing(self)
-    
-  def __str__(self):
-    return self.method_name + str(self.params)    
-    
-class InvocationMemorizer(Invocation):
-  def __call__(self, *params, **named_params):
-    self.params = params
-    self.mock.invocations.append(self)
-    
-    for invocation in self.mock.stubbed_invocations:
-      if self.matches(invocation):
-        return invocation.answers[0].answer()
-    
-    return None
-  
-class InvocationVerifier(Invocation):
-  def __call__(self, *params, **named_params):
-    self.params = params
-    matches = 0
-    for invocation in self.mock.invocations:
-      if self.matches(invocation):
-        matches += 1
-        invocation.verified = True
-  
-    if (self.mock.mocking_mode == 1 and matches != self.mock.mocking_mode):
-      raise VerificationError("\nWanted but not invoked: " + str(self))
-    elif (matches != self.mock.mocking_mode):
-      raise VerificationError("Wanted times: " + str(self.mock.mocking_mode) + ", actual times: " + str(matches))
-  
-class InvocationStubber(Invocation):
-  def __call__(self, *params, **named_params):
-    self.params = params    
-    return AnswerSelector(self)
-  
-class AnswerSelector:
-  def __init__(self, invocation):
-    self.invocation = invocation
-    self.chained_mode = False
-    
-  def thenReturn(self, return_value):
-    return self.__then(Answer(return_value, _RETURNS_))
-    
-  def thenRaise(self, exception):
-    return self.__then(Answer(exception, _THROWS_))
-
-  def __then(self, answer):
-    self.invocation.stubWith(answer, self.chained_mode)     
-    self.chained_mode = True
-    return self      
-
-class Answer:
-  def __init__(self, value, type):
-    self.answers = [[value, type]]
-    self.index = 0
-
-  def current(self):
-    return self.answers[self.index]
-
-  def append(self, answer):
-    self.answers.append(answer)
-
-  def answer(self):
-    answer = self.current()[0] 
-    type = self.current()[1]
-    if self.index < len(self.answers) - 1: self.index += 1
-    if type == _THROWS_: raise answer
-    return answer
-
-class VerificationError(AssertionError):
-  pass
+import static_mocker
+import verification
+from mock import Mock
+from verification import VerificationError
 
 class ArgumentError(Exception):
   pass
-  
-def verify(obj, times=1):
+
+def verify(obj, times=1, atLeast=None, atMost=None, between=None):
+# TODO: refactor this mess below...  
   if times < 0:
     raise ArgumentError("'times' argument has invalid value. It should be at least 0. You wanted to set it to: " + str(times))
+  if len(filter(lambda x: x, [atLeast, atMost, between])) > 1:
+    raise ArgumentError("Sure you know what you are doing? You can set only one of the arguments: 'atLeast', 'atMost' or 'between'.")
+  if (atLeast and atLeast < 1) or atLeast == 0:
+    raise ArgumentError("'atLeast' argument has invalid value. It should be at least 1.  You wanted to set it to: " + str(atLeast))
+  if (atMost and atMost < 1) or atMost == 0:
+    raise ArgumentError("'atMost' argument has invalid value. It should be at least 1.  You wanted to set it to: " + str(atMost))
+  if between:
+    start, end = between
+    if start > end or start < 0:
+      raise ArgumentError("'between' argument has invalid value. It should be both positive values with second number not greater than first e.g. [1, 4] or [0, 3] or [2, 2].  You wanted to set it to: " + str(between))
    
-  if _STATIC_MOCKER_.accepts(obj): mock = _STATIC_MOCKER_.getMockFor(obj)  
+  if static_mocker.INSTANCE.accepts(obj): mock = static_mocker.INSTANCE.getMockFor(obj)  
   else: mock = obj
   
-  mock.mocking_mode = times
+  if atLeast:
+    mock.verification = verification.AtLeast(atLeast)
+  elif atMost:
+    mock.verification = verification.AtMost(atMost)
+  elif between:
+    mock.verification = verification.Between(*between)
+  else:
+    mock.verification = verification.Times(times)
   return mock
 
 def times(count):
@@ -176,17 +42,17 @@ def when(obj):
   #TODO verify obj is a class or a mock
   
   mock = obj
-  if (_STATIC_MOCKER_.accepts(obj)):
+  if (static_mocker.INSTANCE.accepts(obj)):
     mock = Mock()
     mock.mocked_obj = obj
 
-  mock.mocking_mode = _STUBBING_
+  mock.expectStubbing()
   return mock
 
 def unstub():
   """Unstubs all stubbed static methods and class methods"""
   
-  _STATIC_MOCKER_.unstub()
+  static_mocker.INSTANCE.unstub()
 
 def verifyNoMoreInteractions(*mocks):
   for mock in mocks:
@@ -197,7 +63,6 @@ def verifyNoMoreInteractions(*mocks):
 def verifyZeroInteractions(*mocks):
   verifyNoMoreInteractions(*mocks)
       
-#TODO how to generate documentation from python?      
 def any(wanted_type=None):
   """Matches any() argument OR any(SomeClass) argument
      Examples:
--- src/mockito.py
+++ src/mockito.py
@@ -1,9 +1,8 @@
 import matchers
-import static_mocker 
+import static_mocker
+import verification
 from mock import Mock
-
-class VerificationError(AssertionError):
-  pass
+from verification import VerificationError
 
 class ArgumentError(Exception):
   pass
@@ -27,49 +26,14 @@ def verify(obj, times=1, atLeast=None, atMost=None, between=None):
   else: mock = obj
   
   if atLeast:
-    mock.verification = AtLeast(atLeast)
+    mock.verification = verification.AtLeast(atLeast)
   elif atMost:
-    mock.verification = AtMost(atMost)
+    mock.verification = verification.AtMost(atMost)
   elif between:
-    mock.verification = Between(*between)
+    mock.verification = verification.Between(*between)
   else:
-    mock.verification = Times(times)
+    mock.verification = verification.Times(times)
   return mock
-
-class AtLeast(object):
-  def __init__(self, wanted_count):
-    self.wanted_count = wanted_count
-    
-  def verify(self, invocation, actual_count):
-    if actual_count < self.wanted_count: 
-      raise VerificationError("Wanted at least: " + str(self.wanted_count) + ", actual times: " + str(actual_count))
-    
-class AtMost(object):
-  def __init__(self, wanted_count):
-    self.wanted_count = wanted_count
-    
-  def verify(self, invocation, actual_count):
-    if actual_count > self.wanted_count: 
-      raise VerificationError("Wanted at most: " + str(self.wanted_count) + ", actual times: " + str(actual_count))
-
-class Between(object):
-  def __init__(self, wanted_from, wanted_to):
-    self.wanted_from = wanted_from
-    self.wanted_to = wanted_to
-    
-  def verify(self, invocation, actual_count):
-    if actual_count < self.wanted_from or actual_count > self.wanted_to: 
-      raise VerificationError("Wanted between: " + str((self.wanted_from, self.wanted_to)) + ", actual times: " + str(actual_count))
-    
-class Times(object):
-  def __init__(self, wanted_count):
-    self.wanted_count = wanted_count
-    
-  def verify(self, invocation, actual_count):
-    if actual_count != self.wanted_count:
-      raise VerificationError("\nWanted but not invoked: " + str(invocation))
-    elif actual_count != self.wanted_count:
-      raise VerificationError("Wanted times: " + str(self.wanted_count) + ", actual times: " + str(actual_count))
 
 def times(count):
   return count
