affd3e37e914490db980d4cd36423cff0d04759c
Kaste <herr.kaste@googlemail.com>
2009-08-22 00:19:44 +0200

    Useful exception messages for the matchers and a StubbingInvocation-exception
    Fixed a bug in the Contains-matcher because not every argument might have a find method, int's usually don't.

--- mockito/matchers.py
+++ mockito/matchers.py
@@ -11,10 +11,18 @@ class Any(Matcher):
       return isinstance(arg, self.wanted_type)
     else:
       return True
+  
+  def __repr__(self):
+    return "<Any: %s>" % self.wanted_type  
 
 class Contains(Matcher):
   def __init__(self, sub):
     self.sub = sub
       
   def matches(self, arg):
+    if not hasattr(arg, 'find'):
+      return  
     return self.sub and len(self.sub) > 0 and arg.find(self.sub) > -1
+
+  def __repr__(self):
+    return "<Contains: '%s'>" % self.sub  
