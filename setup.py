2aac176ee129f15b6e954605b085f59e14fab5a3
Serhiy Oplakanets <serhiy@oplakanets.com>
2010-02-25 23:15:03 +0200

    Add PyPi classifiers.

--- setup.py
+++ setup.py
@@ -49,5 +49,10 @@ setup(name='mockito',
       maintainer_email='mockito-python@googlegroups.com',
       license='MIT',
       description='Spying framework',
-      long_description='Mockito is a spying framework based on Java library with the same name.'
+      long_description='Mockito is a spying framework based on Java library with the same name.',
+      classifiers=['Development Status :: 4 - Beta',
+                   'Intended Audience :: Developers',
+                   'License :: OSI Approved :: MIT License',
+                   'Topic :: Software Development :: Testing'
+                  ]
 )
