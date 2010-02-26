e755979f1c2d6f227f4b1829c623cd8cc3d9b2a7
Serhiy Oplakanets <serhiy@oplakanets.com>
2010-02-26 17:51:02 +0200

    Fix test script.

--- test.sh
+++ test.sh
@@ -2,15 +2,11 @@
 
 # Run unit tests against multiple versions of python.
 
-cd mockito_test
-
 VERSIONS=( 2.4 2.5 2.6 )
 
 for version in ${VERSIONS[@]}; do
-    python${version} all_tests.py
+    python${version} setup.py test -q
     echo "Test results for version ${version}. Press <enter> to continue."
     read
 done
-
-cd ..
 
