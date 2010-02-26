9b0933cdab824f891b419402a7503c196f832f42
Serhiy Oplakanets <serhiy@oplakanets.com>
2010-02-26 18:37:30 +0200

    Add python3 to tests file.

--- test.sh
+++ test.sh
@@ -2,7 +2,7 @@
 
 # Run unit tests against multiple versions of python.
 
-VERSIONS=( 2.4 2.5 2.6 )
+VERSIONS=( 2.4 2.5 2.6 3 )
 
 for version in ${VERSIONS[@]}; do
     python${version} setup.py test -q
