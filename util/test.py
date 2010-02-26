6e8d11b4a8da78fe3ffd742b00de295bba4680a9
Serhiy Oplakanets <serhiy@oplakanets.com>
2010-02-26 17:41:08 +0200

    Easier API.

--- util/test.py
+++ util/test.py
@@ -7,7 +7,10 @@ class TestLoader(BaseTestLoader):
   def __init__(self):
     BaseTestLoader.__init__(self)
 
-  def loadTestsFromNames(self, names, module):
+  def loadTestsFromName(self, name, module=None):
+    return self.loadTestsFromNames([name])
+
+  def loadTestsFromNames(self, names, module=None):
     suite = TestSuite()
     for test in findTests(dirs=names):
       suite.addTests(super(TestLoader, self).loadTestsFromName(test))
@@ -20,4 +23,6 @@ def findTests(dirs):
     for fileName in os.listdir(dir):
       if pattern.match(fileName):
         yield os.path.join(dir, fileName).replace('.py', '').replace(os.sep, '.')
+
+__all__ = [TestLoader]
 
