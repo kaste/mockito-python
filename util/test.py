df456801612dad4bf6f534a35d339830a84986e3
Serhiy Oplakanets <serhiy@oplakanets.com>
2010-02-26 18:29:30 +0200

    Make test loader python3 compatible.

--- util/test.py
+++ util/test.py
@@ -2,27 +2,32 @@
 # coding: utf-8
 
 from unittest import TestLoader as BaseTestLoader, TestSuite
+import sys
 
 class TestLoader(BaseTestLoader):
   def __init__(self):
     BaseTestLoader.__init__(self)
 
   def loadTestsFromName(self, name, module=None):
-    return self.loadTestsFromNames([name])
+    suite = TestSuite()
+    for test in findTests(name):
+      sys.path.insert(0, name) # python3 compatibility
+      suite.addTests(super(TestLoader, self).loadTestsFromName(test))
+      del sys.path[0] # python3 compatibility
+    return suite
 
   def loadTestsFromNames(self, names, module=None):
     suite = TestSuite()
-    for test in findTests(dirs=names):
-      suite.addTests(super(TestLoader, self).loadTestsFromName(test))
+    for name in names:
+      suite.addTests(self.loadTestsFromName(name))
     return suite
 
-def findTests(dirs):
+def findTests(dir):
   import os, re
   pattern = re.compile('([a-z]+_)+test\.py$')
-  for dir in dirs:
-    for fileName in os.listdir(dir):
-      if pattern.match(fileName):
-        yield os.path.join(dir, fileName).replace('.py', '').replace(os.sep, '.')
+  for fileName in os.listdir(dir):
+    if pattern.match(fileName):
+      yield os.path.join(dir, fileName).replace('.py', '').replace(os.sep, '.')
 
 __all__ = [TestLoader]
 
