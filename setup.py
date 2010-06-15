235c8312ce43d80d3dedede65f2c0ee98f23b8b8
Serhiy Oplakanets <serhiy@oplakanets.com>
2010-06-15 12:36:57 +0300

    Pushing to version 0.3

--- setup.py
+++ setup.py
@@ -15,7 +15,7 @@ if sys.version_info >= (3,):
   extra['use_2to3'] = True
 
 setup(name='mockito',
-      version='0.2.0',
+      version='0.3.0',
       packages=['mockito', 'mockito_test', 'mockito_util'],
       url='http://code.google.com/p/mockito/wiki/MockitoForPython',
       download_url='http://bitbucket.org/szczepiq/mockito-python/downloads/',
