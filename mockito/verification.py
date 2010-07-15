c6206f6503abe7c79d52f48ad03a099c474eead9
Serhiy Oplakanets <serhiy@oplakanets.com>
2010-07-15 13:20:10 +0300

    All error messages start with a new line.

--- mockito/verification.py
+++ mockito/verification.py
@@ -22,7 +22,7 @@ class AtLeast(object):
     
   def verify(self, invocation, actual_count):
     if actual_count < self.wanted_count: 
-      raise VerificationError("Wanted at least: %i, actual times: %i" % (self.wanted_count, actual_count))
+      raise VerificationError("\nWanted at least: %i, actual times: %i" % (self.wanted_count, actual_count))
     
 class AtMost(object):
   def __init__(self, wanted_count):
@@ -30,7 +30,7 @@ class AtMost(object):
     
   def verify(self, invocation, actual_count):
     if actual_count > self.wanted_count: 
-      raise VerificationError("Wanted at most: %i, actual times: %i" % (self.wanted_count, actual_count))
+      raise VerificationError("\nWanted at most: %i, actual times: %i" % (self.wanted_count, actual_count))
 
 class Between(object):
   def __init__(self, wanted_from, wanted_to):
@@ -39,7 +39,7 @@ class Between(object):
     
   def verify(self, invocation, actual_count):
     if actual_count < self.wanted_from or actual_count > self.wanted_to: 
-      raise VerificationError("Wanted between: [%i, %i], actual times: %i" % (self.wanted_from, self.wanted_to, actual_count))
+      raise VerificationError("\nWanted between: [%i, %i], actual times: %i" % (self.wanted_from, self.wanted_to, actual_count))
     
 class Times(object):
   def __init__(self, wanted_count):
