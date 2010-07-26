6b6e4dcfde7bdace4516b06d71314b0ccc3aecf6
Serhiy Oplakanets <serhiy@oplakanets.com>
2010-07-26 13:53:04 +0300

    Pushing to 0.5.0.

--- setup.py
+++ setup.py
@@ -15,7 +15,7 @@ if sys.version_info >= (3,):
   extra['use_2to3'] = True
 
 setup(name='mockito',
-      version='0.4.0',
+      version='0.5.0',
       packages=['mockito', 'mockito_test', 'mockito_util'],
       url='http://code.google.com/p/mockito-python',
       download_url='http://code.google.com/p/mockito-python/downloads/list',
