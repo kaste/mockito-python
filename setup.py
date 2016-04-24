0b5229b06fb4f2d2102881bf28530ba925f22b92
herr kaste <herr.kaste@googlemail.com>
2016-04-25 00:39:44 +0200

    Bump version

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ if sys.version_info >= (3,):
     extra['use_2to3'] = True
 
 setup(name='mockito',
-      version='0.5.3',
+      version='0.6.0',
       packages=['mockito', 'mockito_test', 'mockito_util'],
       url='https://github.com/kaste/mockito-python',
       maintainer='herr.kaste',
