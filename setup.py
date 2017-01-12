1c4aa1cdb662bbd346e035765dd0a18a43f44552
herr kaste <herr.kaste@gmail.com>
2017-01-12 22:55:00 +0100

    Update classifiers in setup.py

--- setup.py
+++ setup.py
@@ -23,6 +23,12 @@ setup(name='mockito',
           'License :: OSI Approved :: MIT License',
           'Topic :: Software Development :: Testing',
           'Programming Language :: Python :: 2',
-          'Programming Language :: Python :: 3'
+          'Programming Language :: Python :: 2.6',
+          'Programming Language :: Python :: 2.7',
+          'Programming Language :: Python :: 3',
+          'Programming Language :: Python :: 3.3',
+          'Programming Language :: Python :: 3.4',
+          'Programming Language :: Python :: 3.5',
+          'Programming Language :: Python :: 3.6',
       ],
       **extra)
