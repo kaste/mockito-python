230239a1ea9661ae22b3dd8689648a3e660c69e2
herr kaste <herr.kaste@gmail.com>
2018-08-31 09:30:04 +0200

    Add 3.7 trove classifier

--- setup.py
+++ setup.py
@@ -36,4 +36,5 @@ setup(name='mockito',
           'Programming Language :: Python :: 3.4',
           'Programming Language :: Python :: 3.5',
           'Programming Language :: Python :: 3.6',
+          'Programming Language :: Python :: 3.7',
       ])
