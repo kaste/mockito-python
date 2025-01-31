96bcd8acc419e496aa157ee749b01d20251b5d5e
herr kaste <herr.kaste@gmail.com>
2025-01-31 15:07:47 +0100

    Let's invent the abstract superclass `RealInvocation`

--- mockito/invocation.py
+++ mockito/invocation.py
@@ -66,12 +66,14 @@ class Invocation(object):
         return "%s(%s)" % (self.method_name, params)
 
 
-class RememberedInvocation(Invocation):
+class RealInvocation(Invocation, ABC):
     def __init__(self, mock, method_name):
-        super(RememberedInvocation, self).__init__(mock, method_name)
+        super(RealInvocation, self).__init__(mock, method_name)
         self.verified = False
         self.verified_inorder = False
 
+
+class RememberedInvocation(RealInvocation):
     def ensure_mocked_object_has_method(self, method_name):
         if not self.mock.has_method(method_name):
             raise InvocationError(
@@ -133,16 +135,11 @@ Stubbed invocations are:
         return None
 
 
-class RememberedProxyInvocation(Invocation):
+class RememberedProxyInvocation(RealInvocation):
     '''Remeber params and proxy to method of original object.
 
     Calls method on original object and returns it's return value.
     '''
-    def __init__(self, mock, method_name):
-        super(RememberedProxyInvocation, self).__init__(mock, method_name)
-        self.verified = False
-        self.verified_inorder = False
-
     def __call__(self, *params, **named_params):
         self._remember_params(params, named_params)
         self.mock.remember(self)
